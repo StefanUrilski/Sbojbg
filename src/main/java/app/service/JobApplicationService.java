72cb5103521fa7302577796de5b93de56408fef6
StefanUrilski <urilskistefan@gmail.com>
2019-11-02 14:41:14 +0200

    JobApplicationService getAllSectors method added.

--- src/main/java/app/service/JobApplicationService.java
+++ src/main/java/app/service/JobApplicationService.java
@@ -2,7 +2,11 @@ package app.service;
 
 import app.domain.models.service.JobServiceModel;
 
+import java.util.List;
+
 public interface JobApplicationService {
 
     void addJob(JobServiceModel jobServiceModel);
+
+    List<String> getAllSectors();
 }
--- src/main/java/app/service/JobApplicationServiceImpl.java
+++ src/main/java/app/service/JobApplicationServiceImpl.java
@@ -1,11 +1,15 @@
 package app.service;
 
 import app.domain.entities.JobApplication;
+import app.domain.entities.enums.Sector;
 import app.domain.models.service.JobServiceModel;
 import app.repository.JobApplicationRepository;
 import org.modelmapper.ModelMapper;
 
 import javax.inject.Inject;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class JobApplicationServiceImpl implements JobApplicationService {
 
@@ -23,5 +27,12 @@ public class JobApplicationServiceImpl implements JobApplicationService {
         JobApplication job = modelMapper.map(jobServiceModel, JobApplication.class);
 
         jobRepository.save(job);
+    }
+
+    @Override
+    public List<String> getAllSectors() {
+        return Arrays.stream(Sector.values())
+                .map(Enum::name)
+                .collect(Collectors.toList());
     }
 }
